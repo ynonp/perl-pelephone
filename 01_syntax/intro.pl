1310c79e8a716663b559dc6eb551f746d9f2e073
ynonperek <ynonperek@gmail.com>
2015-07-09 10:54:40 +0300

    add comments to intro.pl

--- 01_syntax/intro.pl
+++ 01_syntax/intro.pl
@@ -12,67 +12,85 @@ use warnings;
 # Elvis has left the building
 # Turn left at the next building
 
+
+# A variable is defined with the keyword
+# "my"
+# It is scoped to the current block, or
+# if defined outside a block - defined
+# to the entire file
+
+# The variable $global in the example
+# below is scoped to the entire file
 my $global = 10;
-
 {
-	my $name = "ynon";
-	my $number = 10;
+  # Variables in block are scoped to current block
+  # A scalar variable in perl can hold both strings
+  # and numbers. It is marked with the $ sigil
+  my $name = "ynon";
+  my $number = 10;
 
-	print "Hello! My name is $name\n";
-	$number = "twenty five";
+  # prints a line of text (newline must
+  # be specified explicitly)
+  print "Hello! My name is $name\n";
+  $number = "twenty five";
 
-	my $x = "10";
-	my $y = 20;
+  my $x = "10";
+  my $y = 20;
 
-	my $z = $x + $y;
-	$z =    ($x . $y) + 5;
+  # In perl the operation defines how to
+  # treat its operands:
+  # A numeric + treats both operands as numbers
+  # A string concatenation . treats both operands
+  # as strings
+  my $z = $x + $y;
+  $z =    ($x . $y) + 5;
 }
 
 # Create a new random number between 0 and 1
 {
-	my $x = rand();
-	my $y = rand() * 100;
-	my $z = int(rand() * 100);
+  my $x = rand();
+  my $y = rand() * 100;
+  my $z = int(rand() * 100);
 
-	print "x = $x, y = $y, z = $z\n";
+  print "x = $x, y = $y, z = $z\n";
 }
 
 {
-	for ( my $i=0 ; $i < 10; $i++ ) {
-		# do what you think it does
-		# perform loop body 10 times
-		# i = 0
-		# i = 1
-		# i = 2
-		# ...
-		# i = 9
-	}
-	my $x = 0;
+  for ( my $i=0 ; $i < 10; $i++ ) {
+    # do what you think it does
+    # perform loop body 10 times
+    # i = 0
+    # i = 1
+    # i = 2
+    # ...
+    # i = 9
+  }
+  my $x = 0;
 
-	while ( $x < 10 ) {
-		$x = int(rand(50));
-	}
-	print "Found: $x\n";
+  while ( $x < 10 ) {
+    $x = int(rand(50));
+  }
+  print "Found: $x\n";
 
 }
 
 {
-	my $x = int(rand(50));
-	if ( $x % 2 == 0 ) {
-		print "x is even\n";
-	} else {
-		print "x is odd\n";
-	}
+  my $x = int(rand(50));
+  if ( $x % 2 == 0 ) {
+    print "x is even\n";
+  } else {
+    print "x is odd\n";
+  }
 
-	if ( -f "/etc/passwd" ) {
-		print "/etc/passwd is a file\n";
-	}
+  if ( -f "/etc/passwd" ) {
+    print "/etc/passwd is a file\n";
+  }
 
-	if ( 2 < 10 ) {
-		print "yes\n";
-	}
+  if ( 2 < 10 ) {
+    print "yes\n";
+  }
 
-	if ( "foo" == "bar" ) {
-		print "unfortunately, yes...\n";
-	}
+  if ( "foo" == "bar" ) {
+    print "unfortunately, yes...\n";
+  }
 }
